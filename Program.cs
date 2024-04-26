205a1dcbc9c5dfc95b502804535e08856e633e91
franklyn.urey <franklyn.urey@kiumas.net>
2024-04-26 10:05:43 -0600

    S02L026 Clase Program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var apellido = builder.Configuration.GetValue<string>("apellido");
+
+//Inicio de área de los servicios
+
+
+//Fin de área de los servicios
 var app = builder.Build();
+//Inicio de área de los middleware
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => $"Hello World! {apellido}");
 
+//Fin de área de los middleware
 app.Run();
