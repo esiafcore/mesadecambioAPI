e01bd0a02af86a0af3971c2671417b343974f061
franklyn.urey <franklyn.urey@kiumas.net>
2024-05-29 18:55:17 -0600

    S06L100 Validación Personalizada

--- Validaciones/BancosDtoCreateValidator.cs
+++ Validaciones/BancosDtoCreateValidator.cs
@@ -9,13 +9,23 @@ public class BancosDtoCreateValidator : AbstractValidator<BancosDtoCreate>
     {
         RuleFor(x => x.UidCia)
             .NotEmpty().WithMessage("El campo {PropertyName} es requerido");
-
+            
         RuleFor(x => x.Codigo)
             .NotEmpty().WithMessage("El campo {PropertyName} es requerido")
+            .Must(FnxCodigoIncorrecto).WithMessage("El campo {PropertyName} es incorrecto")
             .MaximumLength(10).WithMessage("La longitud máxima de caracteres permitidos para el campo {PropertyName} es {MaxLength}");
 
         RuleFor(x => x.Descripci)
             .NotEmpty().WithMessage("El campo {PropertyName} es requerido");
+    }
 
+    private static bool FnxCodigoIncorrecto(string valor)
+    {
+        if (string.IsNullOrWhiteSpace(valor))
+        {
+            return true;
+        }
+        valor = valor.Trim();
+        return (!".-".Contains(valor));                    
     }
 }
