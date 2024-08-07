f9b510a41b1c6e06a64174b539e050b9773e8821
franklyn.urey <franklyn.urey@kiumas.net>
2024-08-07 14:26:45 -0600

    IMP - Add Field AmountComission and Bank Code Target

--- Entidades/XanesN8/DepositsList.cs
+++ Entidades/XanesN8/DepositsList.cs
@@ -3,8 +3,8 @@
 public class DepositsList
 {
     public int Id { get; set; }
-    public int CompanyId { get; set; }
     public int ParentId { get; set; }
+    public int CompanyId { get; set; }
     public int TypeNumeral { get; set; }
     public int QuotationDetailtype { get; set; }
     public DateTime DateTransa { get; set; }
@@ -14,14 +14,14 @@ public class DepositsList
     public string BusinessExecutiveCode { get; set; } = null!;
     public string CurrencyTransactionCode { get; set; } = null!;
     public string CurrencyDepositCode { get; set; } = null!;
-    public decimal ExchangeRateReal { get; set; }
+    public string BankCode { get; set; } = null!;
     public decimal ExchangeRateTransa { get; set; }
+    public decimal ExchangeRateReal { get; set; }
+    public decimal ExchangeRateOfficialTransa { get; set; }
+    public decimal ExchangeRateOfficialReal { get; set; }
     public decimal AmountTransaction { get; set; }
     public decimal AmountDepositTotal { get; set; }
-    public decimal AmountDepositDetailMonbas { get; set; }
-    public decimal AmountDepositDetailMonfor { get; set; }
-    public decimal AmountDepositDetailMonxtr { get; set; }
-
+    public decimal AmountDepositDetail { get; set; }
     public bool IsClosed { get; set; }
     public bool IsBank { get; set; }
     public bool IsPayment { get; set; }
--- Entidades/XanesN8/TransfersList.cs
+++ Entidades/XanesN8/TransfersList.cs
@@ -3,8 +3,8 @@
 public class TransfersList
 {
     public int Id { get; set; }
-    public int CompanyId { get; set; }
     public int ParentId { get; set; }
+    public int CompanyId { get; set; }
     public int TypeNumeral { get; set; }
     public int QuotationDetailtype { get; set; }
     public DateTime DateTransa { get; set; }
@@ -14,13 +14,16 @@ public class TransfersList
     public string BusinessExecutiveCode { get; set; } = null!;
     public string CurrencyTransactionCode { get; set; } = null!;
     public string CurrencyTransferCode { get; set; } = null!;
+    public string BankCodeSource { get; set; } = null!;
+    public string BankCodeTarget { get; set; } = null!;
     public decimal ExchangeRateReal { get; set; }
     public decimal ExchangeRateTransa { get; set; }
+    public decimal ExchangeRateOfficialTransa { get; set; }
+    public decimal ExchangeRateOfficialReal { get; set; }
     public decimal AmountTransaction { get; set; }
     public decimal AmountTransferTotal { get; set; }
-    public decimal AmountTransferDetailMonbas { get; set; }
-    public decimal AmountTransferDetailMonfor { get; set; }
-    public decimal AmountTransferDetailMonxtr { get; set; }
+    public decimal AmountTransferDetail { get; set; }
+    public decimal AmountCommission { get; set; }
 
     public bool IsClosed { get; set; }
     public bool IsBank { get; set; }
