99071e5005aa17431b969472f306ad6da79e12f0
franklyn.urey <franklyn.urey@kiumas.net>
2024-09-11 14:04:10 -0600

    La mancura de junior

---
+++ Validaciones/ConsecutivosCntDetalleDtoUpdateValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using XanesN8.Api.DTOs.eSiafN4;
+using XanesN8.Api.Repositorios.eSiafN4;
+
+namespace XanesN8.Api.Validaciones;
+
+public class ConsecutivosCntDetalleDtoUpdateValidator : AbstractValidator<ConsecutivosCntDetalleDtoUpdate>
+{
+    public ConsecutivosCntDetalleDtoUpdateValidator(IRepositorioConsecutivoCntDetalle repo
+        , IHttpContextAccessor httpContextAccessor)
+    {
+        var valorRutaId = httpContextAccessor.HttpContext?.Request.RouteValues["id"];
+        Guid id = Guid.Empty;
+
+        if (valorRutaId is string valorString)
+        {
+            Guid.TryParse(valorString, out id);
+        }
+
+        RuleFor(x => x.UidCia)
+            .NotEmpty().WithMessage("El campo {PropertyName} es requerido");
+
+        RuleFor(x => x.NombreCampo)
+            .NotEmpty().WithMessage("El campo {PropertyName} es requerido");
+    }
+
+    private static bool FnxCodigoIncorrecto(string valor)
+    {
+        if (string.IsNullOrWhiteSpace(valor))
+        {
+            return true;
+        }
+        valor = valor.Trim();
+        return !".-".Contains(valor);
+    }
+}
