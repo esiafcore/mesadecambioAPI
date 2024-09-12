a2b40ed6683ef1fa6cf91d8018eabef7ab134e2a
AUrey <angel123urey@gmail.com>
2024-09-11 22:53:19 -0600

    IMP Modulos

---
+++ Entidades/eSiafN4/modulos.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XanesN8.Api.Entidades.eSiafN4
+{
+    public partial class Modulos {
+
+        [Key]
+        [Required()]
+        public Guid UidRegist { get; set; }
+
+        [Required()]
+        public Guid UidCia { get; set; }
+
+        [StringLength(5)]
+        [Required()]
+        public string Codigo { get; set; }
+
+        [Required()]
+        public int Numero { get; set; }
+
+        [StringLength(75)]
+        [Required()]
+        public string Descripci { get; set; }
+
+        [Required()]
+        public long ConsecutivoTransa { get; set; }
+
+        [Required()]
+        public long ConsecutivoTransaTemporal { get; set; }
+
+        [Required()]
+        public long ConsecutivoConta { get; set; }
+
+        [Required()]
+        public DateTime CreFch { get; set; }
+
+        [StringLength(75)]
+        [Required()]
+        public string CreUsr { get; set; }
+
+        [StringLength(75)]
+        [Required()]
+        public string CreHsn { get; set; }
+
+        [StringLength(10)]
+        [Required()]
+        public string CreHid { get; set; }
+
+        [StringLength(48)]
+        [Required()]
+        public string CreIps { get; set; }
+
+        [Required()]
+        public DateTime ModFch { get; set; }
+
+        [StringLength(75)]
+        [Required()]
+        public string ModUsr { get; set; }
+
+        [StringLength(75)]
+        [Required()]
+        public string ModHsn { get; set; }
+
+        [StringLength(10)]
+        [Required()]
+        public string ModHid { get; set; }
+
+        [StringLength(48)]
+        [Required()]
+        public string ModIps { get; set; }
+
+        [StringLength(75)]
+        [Required()]
+        public string DescripciFor { get; set; }
+
+        //public virtual IList<ModulosDocumentos> modulosdocumentos { get; set; }
+
+        #region Extensibility Method Definitions
+
+        partial void OnCreated();
+
+        #endregion
+    }
+
+}
